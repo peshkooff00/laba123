7d4bd86197554f5561a003fbc588e3d4f31f9116
Max <56516173+peshkooff00@users.noreply.github.com>
2019-11-20 00:30:38 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cv / Max Skrypnyk</title>
+    <link rel="stylesheet" href="styles/bootstrap.css">
+    <link rel="stylesheet" href="styles/fonts.css">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+<header class="header">
+    <div class="filter">
+        <div class="header--top">
+            <div class="container">
+                <div class="row justify-content-between">
+                    <div class="col-12 justify-content-center header--logo d-flex align-items-center">
+                        My CV
+                    </div>                                       
+                </div>
+            </div>
+        </div>
+        <div class="header--bot">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3 col-md-5 col-sm-6 col-12 header--photo">
+                        <img src="img/photo1.jpg" alt="">
+                    </div>
+                    <div class="col-lg-4 col-md-5 col-sm-6 col-12 d-flex justify-content-center justify-content-sm-start text-sm-left text-center align-items-center">
+                        <div class="header--caption">
+                                <h5>Alecksey Shaina</h6>
+                                <p>Penetretion Tester</p>
+                                <ul>
+                                    <li>Phone number : <span class="caption-info">+380953461360</span></li>
+                                    <li>Email : <span class="caption-info">alecksey778@gmail.com</span></li>
+                                    <li>Telegram : <a href="https://t.me/m19203" class="caption-info">t.me/m19s03</a></li>
+                                </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="about-me">
+    <div class="container">
+        <div class="row about--info">
+            <div class="col-12">
+                <h3>Обо мне</h3>
+                <hr>
+                <p>
+                    Здравствуйте, меня зовут Алексей , я учусь на 3 курсе Киевского национального университета имени Тараса Шевченко, занимаюсь тестированием web приложений
+                    на уязвимости,коммуникабельный,умею работать в команде , быстро обучаем, планирую и дальше развиваться в данной сфере.
+                </p>
+            </div>
+        </div>
+        <div class="row about--courses">
+            <div class="col-12">
+                <h5>Курсы:</h5>
+                <hr>
+                <div class="about--courses-item">
+                    <div class="row">
+                        <div class="col-6">
+                            <h6>Codebyschool WAPT offencive security</h6>
+                            <p>Python Developer</p>
+                        </div>
+                        <div class="col-6 text-right">
+                            <span class="date">Sep - Oct 2019</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h5>Education</h5>
+                <hr>
+            </div>
+        </div>
+        <div class="row education">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="education-item">
+                            <div class="row">
+                                <div class="col-6">
+                                    <h6>Kyiv National University</h6>
+                                    <p>Faculty of information technologies - Cybersecurity</p>
+                                </div>
+                                <div class="col-6 text-right">
+                                    <span class="date">2017 - till now</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               <div class="row">
+                   <div class="col-12">
+                        <div class="education-item">
+                            <div class="row">
+                                <div class="col-6">
+                                    <h6>Энергодарский многопрофильный лицей</h6>
+                                </div>
+                                <div class="col-6 text-right">
+                                    <span class="date">2006 - 2017</span>
+                                </div>
+                            </div>
+                        </div>
+                   </div>
+               </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="skills">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <h3>Tech Skills</h3>
+                <hr>
+                <div class="skills-item">Python</div>
+                <div class="skills-item">Python(Django)</div>
+                <div class="skills-item">Administration Windows</div>
+                <div class="skills-item">Administration Linux</div>
+            </div>
+            <div class="col-sm-6">
+                <h3>Other Skills</h3>
+                <hr>
+                <div class="skills-item">Utilities testing of vournebilities </div>
+                <div class="skills-item">Ruby</div>
+                <div class="skills-item">HTML</div>
+                <div class="skills-item">CSS3</div>
+                <div class="skills-item">Borland Delphi 9</div>
+                <div class="skills-item">Pascal</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- <section class="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>Contact me</h3>
+                <form id="contact-form">
+                    <input type="text" placeholder="Your name" name="name">
+                    <input type="email" placeholder="Email" name="email">
+                    <textarea name="message" placeholder="Message..."></textarea>
+                    <button class="btn btn-mx-default" type="submit">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section> -->
+
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-12 text-sm-left text-center">
+                <h6>Alecksey Shaina</h6>
+            </div>
+            <div class="col-sm-6 text-sm-right text-center col-12">
+                2019
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://smtpjs.com/v3/smtp.js"></script>
+<script src="js/script.js"></script>    
+
+</body>
+</html>
